1d5714e3eb0a93d3ced11a2e2570a50bf79cc315
SantiZapata <santiago050505zapata@gmail.com>
2025-12-18 21:10:52 -0300

    finalizar compra add

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-let formulario = document.getElementById("form-usuario");
+// Simulador de E-Commerce de productos tecnologicos.
+
+const formulario = document.getElementById("form-usuario");
 
 if(formulario){
-    formulario.addEventListener("submit", (e) => {
+    formulario.addEventListener("submit", async (e) => {
         e.preventDefault();
     
         let nombre = document.getElementById("username").value.trim(); 
@@ -9,17 +11,35 @@ if(formulario){
         let email = document.getElementById("email").value.trim();
     
         if (!nombre || !edad || !email) {
-            alert("¡Error! Todos los campos son obligatorios.");
+            Swal.fire({
+                icon: "error",
+                title: "Lo sentimos.",
+                text: "Todos los campos son obligatorios!",
+                showConfirmButton: false,
+                timer: 1000
+            })
             return;
         }
     
         if (edad < 18) {
-            alert("Debes ser mayor de 18 años para registrarte.");
+            Swal.fire({
+                icon: "error",
+                title: "Lo sentimos.",
+                text: "Debes ser mayor de 18 años para ingresar.",
+                showConfirmButton: false,
+                timer: 1000
+            })
             return;
         }
     
         if (!email.includes("@")) {
-            alert("El email no tiene un formato válido (debe contener '@').");
+            Swal.fire({
+                icon: "error",
+                title: "Lo sentimos.",
+                text: "El email no tiene un formato válido (debe contener '@').",
+                showConfirmButton: false,
+                timer: 1500
+            });
             return;
         }
     
@@ -31,38 +51,35 @@ if(formulario){
     
         sessionStorage.setItem('usuarioRegistrado', JSON.stringify(datosUsuario));
     
-        alert("Registro exitoso! Bienvenido/a!.");
-    
-        window.location.href = 'pages/home.html';
+        await Swal.fire({
+        icon: "success",
+        title: `Bienvenido ${datosUsuario.username}!`,
+        text: "Lo estabamos esperando!",
+        showConfirmButton: true,
+        confirmButton: "Continuar"
+        });
+            window.location.href = 'pages/home.html';
     });
 }
 
-let usuarioRecuperado = null;
+function cargarDatosUser (){
+    const datos = sessionStorage.getItem('usuarioRegistrado')
+    if(!datos) return;
+    
+    const usuario = JSON.parse(datos);
+    
+    const usernameHome = document.getElementById("username-home");
+    const usernameAcc = document.getElementById("username-acc")
+    const ageAcc = document.getElementById("age-acc");
+    const emailAcc = document.getElementById("email-acc");
 
-let usernameHome = document.getElementById("username-home");
-if(usernameHome){
-    usuarioRecuperado = JSON.parse(sessionStorage.getItem('usuarioRegistrado'))
+    if (usernameHome) usernameHome.innerText = usuario.username;
+    if (usernameAcc) usernameAcc.innerText = `Nombre de usuario: ${usuario.username}`;
+    if(ageAcc) ageAcc.innerText = `Edad: ${usuario.edad} años`;
+    if(emailAcc) emailAcc.innerText = `Correo electronico: ${usuario.email}`
 }
 
-if (usuarioRecuperado){
-    const {username, edad, email} = usuarioRecuperado;
-    usernameHome.innerText = `${usuarioRecuperado.username}` 
-}
-
-let usernameAcc = document.getElementById("username-acc")
-if(usernameAcc){
-    usernameAcc.innerText = `Nombre de usuario: ${usuarioRecuperado.username}`
-}
-
-let ageAcc = document.getElementById("age-acc");
-if(ageAcc){
-    ageAcc.innerText = `Edad: ${usuarioRecuperado.edad} años`;
-}
-
-let emailAcc = document.getElementById("email-acc");
-if(emailAcc){
-    emailAcc.innerText = `Correo electronico: ${usuarioRecuperado.email}`
-}
+cargarDatosUser();
 
 class Producto {
     constructor (id, marca, modelo, precio, img) {
@@ -101,12 +118,12 @@ for (let i = 0; i < productos.length; i++){
 }
 
 let miCarrito = JSON.parse(sessionStorage.getItem("miCarrito")) || [];
-
 const contenedorCarrito = document.getElementById("shopping-cart");
 const carritoTotal = document.getElementById("shopping-cart-total");
 const contadorCarrito = document.getElementById("contador-carrito");
 const mensajeVacio = document.getElementById("shopping-cart-message");
 const tituloCarrito = document.getElementById("shopping-cart-title");
+const botones = document.querySelectorAll(".product-button");
 
 function agregarAlCarrito(producto){
     miCarrito.push(producto);
@@ -118,10 +135,19 @@ function agregarAlCarrito(producto){
     contadorCarrito.innerText = miCarrito.length;
 };
 
-const botones = document.querySelectorAll(".product-button");
-
 botones.forEach((boton, index) => {
     boton.addEventListener("click", () => {
+    Toastify({
+        text: `${productos[index].marca} ${productos[index].modelo} ha sido agregado al carrito.`,
+        duration: 3000,
+        direction:"../pages/micarrito.html",
+        gravity: "bottom",
+        position: "right",
+        style:{
+            background: "linear-gradient(to right, #0000a2, #0000ff)",
+            borderRadius: "1rem"
+        }
+    }).showToast();
         agregarAlCarrito(productos[index]);
         contadorCarrito.innerText = `Mi carrito(${miCarrito.length})`;
     });
@@ -133,7 +159,6 @@ function activarBotonesEliminar() {
     botonesEliminar.forEach((boton) => {
         boton.addEventListener("click", () => {
             const index = boton.getAttribute("data-index");
-
             eliminardelCarrito(index);
             contadorCarrito.innerText = `Mi carrito(${miCarrito.length})`;
         })
@@ -141,6 +166,19 @@ function activarBotonesEliminar() {
 }
 function eliminardelCarrito(index) {
     miCarrito.splice(index, 1);
+    console.log(eliminardelCarrito);
+    
+    Toastify({
+                text: `Has eliminado ${productos[index].marca} ${productos[index].modelo} del carrito.`,
+                duration: 3000,
+                direction:"../pages/micarrito.html",
+                gravity: "bottom",
+                position: "right",
+                style:{
+                    background: "linear-gradient(to right, #0000a2, #0000ff)",
+                    borderRadius: "1rem"
+        }
+    }).showToast();
     sessionStorage.setItem("miCarrito", JSON.stringify(miCarrito));
     actualizarCarrito();
 }
@@ -181,10 +219,79 @@ function actualizarCarrito(){
     </div>`;
     
     contenedorCarrito.appendChild(totalDiv);
+    const botonFinalizar = document.getElementById("shopping-cart-button")
+    if (botonFinalizar){
+        botonFinalizar.addEventListener("click", finalizarCompra)
+    }
     activarBotonesEliminar();
 }
 
 if (contenedorCarrito) {
     actualizarCarrito();
     contadorCarrito.innerText = `Mi carrito(${miCarrito.length})`;
+} else {
+    contadorCarrito.innerText = `Mi carrito(${miCarrito.length})`;
+}
+
+const procesoDePago = () => {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            const exito = Math.random() > 0.5;
+            if(exito){
+                resolve ({estado: "aprobado", transaccionId: "TX-12345"});
+            } else {
+                reject(new Error ("Fondos insuficientes o error de conexion."))
+            }
+        }, 2500);
+    })
+}
+
+async function finalizarCompra () {
+    if (miCarrito.length === 0){
+        Swal.fire("Carrito vacio","Agrega algo antes de comprar", "warning");
+        return;
+    }
+
+    const {isConfirmed} = await Swal.fire({
+        title: "Confirmar pedido?",
+        text: `Estas por pagar un total de $${miCarrito.reduce((acc,p) => acc + p.precio, 0)}`,
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonText: "Si, pagar ahora",
+        cancelButtonText: "Seguir comprando"
+    });
+
+    if (!isConfirmed) return;
+
+    try {
+        Swal.fire({
+            title: "Procesando pago...",
+            text: "Estamos validando con tu banco, no cierres la ventana.",
+            allowOutsideClick: false,
+            didOpen: () => {
+                Swal.showLoading();
+            }
+        });
+
+        const resultado = await procesoDePago(miCarrito);
+        console.log("Respuesta del servidor:", resultado);
+
+        await Swal.fire ({
+            icon: "success",
+            title: "Compra realizada!",
+            text: `Tu numero de ticket es: ${resultado.transaccionId}`
+        });
+
+        miCarrito = [];
+        sessionStorage.removeItem ("miCarrito");
+        actualizarCarrito();
+        contadorCarrito.innerText = `Mi carrito (0)`
+    } catch (error) {
+        console.error("Falla en la compra: ", error.message);
+        Swal.fire({
+            icon: "error",
+            title: "Pago rechazado",
+            text: error.message,
+        })
+    }
 }
